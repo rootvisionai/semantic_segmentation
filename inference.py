dab669b645b5c36403a3b34778e3bb649bf4f341
Tekin Evrim Ozmermer <evrimozmermer@hotmail.com>
2023-03-03 15:45:27 +0200

    added inference.py, but it will be developed further later.

--- inference.py
+++ inference.py
@@ -1,70 +1,79 @@
-from models.unet import SEQUNET
-from datasets.pascal_voc import get_transforms, VOC_COLORMAP_OLD
-from utils import load_config
-from models.knn import KNN
-from datasets.pascal_voc import VOC_COLORMAP_NEW, VOC_CLASSES
-
 import os
+import time
+
 import torch
 import torchvision
-import glob
+import numpy as np
+import collections
+import shutil
+import sys
 import cv2
-from utils import convert_binary_to_rgb, BCEDiceLoss
 
-def run(cfg, path_to_images, steps=None):
-    # get model
-    model = SEQUNET(
-        input_channels=cfg.model.input_channels,
-        init_dim=cfg.model.init_dim,
-        dim=cfg.model.dim,
-        resnet_block_groups=cfg.model.resnet_block_groups,
-        dim_mults=cfg.model.dim_mults,
-        out_dim=len(VOC_CLASSES),
-        steps=cfg.model.steps,
-        loss_function=BCEDiceLoss(),
-        learning_rate=cfg.training.learning_rate,
-        optimizer="AdamW",
-        device=cfg.training.device,
-    )
-    model.to(cfg.training.device)
+import models
+from utils import load_config
 
-    # load checkpoint if exists
-    checkpoint_path = f"task[{cfg.training.task}]_init_dim[{cfg.model.init_dim}]_dim[{cfg.model.dim}]_resnet_block_groups" + \
-                      f"[{cfg.model.resnet_block_groups}]_step[{cfg.model.steps}]_input[{cfg.data.input_size}]"
-    checkpoint_path = os.path.join("checkpoints", checkpoint_path)
-    if not os.path.isdir(checkpoint_path):
-        os.makedirs(checkpoint_path)
-    checkpoint_path = os.path.join(checkpoint_path, "ckpt.pth")
-    if os.path.isfile(checkpoint_path):
-        model.load_checkpoint(checkpoint_path, device=cfg.training.device)
 
-    # define KNN
-    pix_knn = KNN(colors=VOC_COLORMAP_NEW, classes=VOC_CLASSES)
-
-    # get images
-    _, transform = get_transforms(cfg.data, eval=True)
-    image_paths = glob.glob(os.path.join(path_to_images, "*.jpg"))
-    print("INFERENCE IMAGES:")
-    for p in image_paths:
-        print(f"---> {p}")
-    images = [cv2.imread(p) for p in image_paths]
-    images = [cv2.cvtColor(img, cv2.COLOR_BGR2RGB) for img in images]
-    images = [transform(image=img)["image"]/255 for img in images]
+def preprocess(image_paths, size=(256, 256)):
+    images = []
+    out_sizes = []
+    for path in image_paths:
+        image = cv2.imread(path) # load
+        image = torch.from_numpy(image) # to uint8 tensor
+        out_sizes.append(tuple(image.shape[0:2]))
+        image = image.permute(2, 0, 1).unsqueeze(0) / 255 # to [0,1] range bs 1 float32 tensor
+        image = torch.nn.functional.interpolate(image, size=size, mode="nearest")
+        images.append(image[0])
     images = torch.stack(images, dim=0)
+    return images, out_sizes
 
-    model.eval()
-    with torch.no_grad():
-        # images = images.sum(1)/3
-        # images = torch.stack([images, images, images], dim=1)
-        # preds = model.infer(images.to(cfg.training.device), steps=steps if steps else cfg.model.steps, save_intermediate=True).cpu()
-        # preds_knn = pix_knn(preds.cpu())
-        preds = model.infer(images.to(cfg.training.device), task=cfg.training.task, activation=torch.sigmoid).cpu()
-        preds = convert_binary_to_rgb(VOC_COLORMAP_OLD, preds.cpu())
-
-    for i, path in enumerate(image_paths):
-        torchvision.utils.save_image(preds[i].unsqueeze(0).cpu(), path.replace(".jpg", "_pred.jpg"))
-        # torchvision.utils.save_image(preds_knn[i].unsqueeze(0).cpu(), path.replace(".jpg", "_pred_knn.jpg"))
 
 if __name__ == "__main__":
-    cfg = load_config("./config.yml")
-    run(cfg=cfg, steps=30, path_to_images="test_images")
+    checkpoint_dir = "arch[UNetResnet]-" \
+                     "num_classes[11]-" \
+                     "in_channels[3]-" \
+                     "backbone[resnet50]-" \
+                     "output_stride[16]-" \
+                     "freeze_bn[False]-" \
+                     "freeze_backbone[False]"
+
+    cfg = load_config(os.path.join(
+        "checkpoints",
+        f"{checkpoint_dir}",
+        "config.yml"
+    ))
+
+    # get model
+    model = models.load(cfg)
+    model.to(cfg.inference.device)
+    model.eval()
+
+    checkpoint_path = os.path.join(
+        "checkpoints",
+        f"{checkpoint_dir}",
+        "ckpt.pth"
+    )
+    last_epoch = model.load_checkpoint(checkpoint_path, device=cfg.training.device)
+
+    print(f"Loaded model: {checkpoint_path}\n"
+          f"Last epoch: {last_epoch}")
+
+    image_paths = [os.path.join(cfg.inference.input_dir, elm) for elm in os.listdir(cfg.inference.input_dir)]
+    images, out_sizes = preprocess(image_paths, size=(cfg.data.input_size, 256))
+    print(f"Running inference in images: {images.shape}")
+    images = images.to(cfg.inference.device)
+    preds = model.infer(images)
+
+    for bs in range(preds.shape[0]):
+        pred = preds[bs].unsqueeze(0)
+        pred += 1
+        pred /= 10
+        pred -= pred.min()
+        pred = torch.nn.functional.interpolate(pred, size=out_sizes[bs])
+
+        save_dir = image_paths[bs].replace(cfg.inference.input_dir, cfg.inference.output_dir)
+        save_dir = save_dir.replace(".jpg", f"_{bs}.png")
+        torchvision.utils.save_image(pred, save_dir)
+        print(f"Saved {save_dir}")
+
+
+
